71390859787d8db3023b63417af11eb14ecc4f9c
Sibin Mohanan <sibinkyem@gmail.com>
2022-05-03 08:46:44 +0530

    directive mock failure fix

--- src/app/core/directives/autofocus.directive.spec.ts
+++ src/app/core/directives/autofocus.directive.spec.ts
@@ -2,7 +2,10 @@ import { AutofocusDirective } from './autofocus.directive';
 
 describe('AutofocusDirective', () => {
   it('should create an instance', () => {
-    const directive = new AutofocusDirective();
+    let elRefMock = {
+      nativeElement: document.createElement('div')
+    };
+    const directive = new AutofocusDirective(elRefMock);
     expect(directive).toBeTruthy();
   });
 });
